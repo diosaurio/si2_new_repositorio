92fc811d393cb51484c4ca5e79aa7be0b20bfa18
lin.qi <lin.qi@estudiante.uam.es>
2026-02-24 23:32:54 +0100

    Create frontend

---
+++ P1-ws-frontend/read_1000_entries_from_db_ORM.py
@@ -0,0 +1,34 @@
+# connect to the database, read the first 1000 entries
+# then perform 1000 queries retrieving each one of the entries
+# one by one. Measure the time requiered for the 1000 queries
+
+
+# Configurar Django
+import os, django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "visaSite.settings")
+django.setup()
+import psycopg2
+import time
+from visaAppWSFrontend.models import Tarjeta
+
+try:
+    # Conexion a la base de datos
+    tarjetas = Tarjeta.objects.all()[:1000]
+
+    # Medir el tiempo de inicio
+    start_time = time.time()
+
+    # Realizar busquedas una a una
+    for tarjeta in tarjetas:
+        id_value = tarjeta.numero  # Suponiendo que la primera columna es el ID
+        tarjeta_encontrada = Tarjeta.objects.get(pk=id_value)
+
+    # Medir el tiempo de finalizacion
+    end_time = time.time()
+
+    # Mostrar los resultados
+    print(f"Tiempo invertido en buscar las 1000 entradas una a una: {end_time - start_time:.6f} segundos")
+
+except Exception as e:
+    print(f"Error: {e}")
+
---
+++ P1-ws-frontend/visaAppWSFrontend/pagoDB.py
@@ -0,0 +1,100 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# author: rmarabini
+"Interface with the dataabse"
+import requests
+from django.conf import settings
+from urllib.parse import urljoin 
+
+def get_api_url(endpoint):
+    base = settings.RESTAPIBASEURL.rstrip('/') + '/'
+    return urljoin(base, endpoint.strip('/') + '/')
+
+
+def verificar_tarjeta(tarjeta_data):
+    """ Check if the tarjeta is registered 
+    :param tarjeta_dict: dictionary with the tarjeta data
+                       (as provided by TarjetaForm)
+    :return True or False if tarjeta_data is not valid
+    """
+    if bool(tarjeta_data) is False:
+       return False
+       
+    api_url = get_api_url('tarjeta')
+
+    try:
+        response = requests.post(api_url, json=tarjeta_data, timeout=10)
+
+        response.raise_for_status() 
+    
+        return True
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error: {err}")
+        return False
+    except requests.exceptions.Timeout:
+        print("Timeout")
+        return False
+
+
+def registrar_pago(pago_dict):
+    """ Register a payment in the database
+    :param pago_dict: dictionary with the pago data (as provided by PagoForm)
+      plus de tarjeta_id (numero) of the tarjeta
+    :return new pago info if succesful, None otherwise
+    """
+    api_url = get_api_url('pago')
+    try:
+        response = requests.post(api_url, json=pago_dict, timeout=10)
+
+        response.raise_for_status()
+        pago = response.json()
+        return pago
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error: {err}")
+        return None
+    except requests.exceptions.Timeout:
+        print("Timeout")
+        return None
+
+
+def eliminar_pago(idPago):
+    """ Delete a pago in the database
+    :param idPago: id of the pago to be deleted
+    :return True if succesful,
+     False otherwise
+     """
+    api_url = get_api_url(f'pago/{idPago}')
+    try:
+        response = requests.delete(api_url, timeout=10)
+
+        response.raise_for_status()
+        return True
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error: {err}")
+        return False
+    except requests.exceptions.Timeout:
+        print("Timeout")
+        return False
+
+
+def get_pagos_from_db(idComercio):
+    """ Gets pagos in the database correspondint to some idComercio
+    :param idComercio: id of the comercio to get pagos from 
+    :return list of pagos found
+     """
+    api_url = get_api_url(f'comercio/{idComercio}')
+    try:
+        response = requests.get(api_url, timeout=10)
+
+        response.raise_for_status()
+        pagos = response.json()
+        return pagos
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error: {err}")
+        return []
+    except requests.exceptions.Timeout:
+        print("Timeout")
+        return []
